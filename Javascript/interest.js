662025e2a94abae491816c0602feb930f30b5c99
shivani <s.suresh.kedare@accenture.com>
2026-06-26 14:45:08 +0530

    task 12

---
+++ Javascript/interest.js
@@ -0,0 +1,4 @@
+import { cInterest } from "./main.js";
+
+const A = cInterest(1000, 0.05, 12, 5);
+console.log(A);
---
+++ Javascript/main.js
@@ -0,0 +1,9 @@
+function cInterest(P, r, n, t) {
+    
+    return P * ((1 + r / n)**(n * t));
+
+}
+
+export {
+    cInterest
+}
